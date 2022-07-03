dad91cb8fc79a69847af7e02fb7f8c264d36e025
Heitor Bardemaker A. Bisneto <hbarbisneto@gmail.com>
2022-07-03 03:00:59 -0300

    Adivinhe

--- Adivinhe.py
+++ Adivinhe.py
@@ -1,93 +1,83 @@
+from ctypes.wintypes import PINT
 from colorama import *
 from random import randint
 from time import sleep
 import sys
-import os 
+import os
 
 # Definindo funções
-	
 def restart_program():
     python = sys.executable
     os.execl( python, python, * sys.argv )
-    
+
 def limpar_terminal():
 	os.system('cls' if os.name == 'nt' else 'clear')
-	
-def espaco():
-	print(' '  *30 )
 
 # Título
-
-espaco()
-print(Fore.CYAN + '\t ADIVINHE ')
+# print()
+print(Fore.CYAN + '\t ADIVINHE')
 print(Fore.CYAN + '_'  *30)
-print(' ' *30)
+print()
 
 # Introdução
 
-print(Fore.WHITE + 'Vou pensar em um número de 1 a 100, tente adivinhar... ')
+print(Fore.WHITE + 'Vou pensar em um número de 1 a 100. Tente adivinhar...')
 print(Fore.WHITE + 'Você tem 7 tentativas: ')
-espaco()
-print(Fore.CYAN + 'PROCESSANDO... ')
-espaco()
+print()
+print(Fore.CYAN + 'PROCESSANDO...')
+print()
 
 # Gerador de informações
 
-número_aleatório = randint( 1,100 )
+numero_aleatorio = randint(1,100)
 tentativas = 0
 palpite = None
 
 # Função principal
 
-while palpite != número_aleatório:
-	sleep( 0.5 )
+while palpite != numero_aleatorio:
+	sleep(0.5)
 	palpite = int(input(Fore.WHITE + Style.NORMAL + 'Digite seu palpite: '))
-	espaco()
 	tentativas = tentativas + 1
 	
 # Caso acerte
 	
-	if palpite == número_aleatório:
-		espaco()
-		print(Fore.GREEN + Style.BRIGHT + 'PARABÉNS!  Você me venceu em {} tentativas '.format( tentativas ))
+	if palpite == numero_aleatorio:
+		print(Fore.GREEN + Style.BRIGHT + 'PARABÉNS! Você me venceu em {} tentativas'.format(tentativas))
 		print(Fore.CYAN + Style.NORMAL + '_' *30)
-		sleep( 0.6 )
-		espaco()
+		sleep(0.6)
 		break;
 		
 # Tentativas
 		
-	elif tentativas >= 7 and palpite != número_aleatório:
-		espaco()
-		print(Fore.RED + Style.BRIGHT + 'Suas chances foram excedidas!! ')
+	elif tentativas >= 7 and palpite != numero_aleatorio:
+		print(Fore.RED + Style.BRIGHT + 'Suas chances foram excedidas!!!')
 		print(Fore.CYAN + Style.NORMAL + '_'  *30)
-		espaco()
-		sleep( 0.6 )
+		print()
+		sleep(0.6)
 		break;
 	
 # Chutes
 		
-	elif palpite > número_aleatório:
-		print(Fore.RED + Style.BRIGHT + 'Hmm... Chutou alto demais!! tente novamente: ')
-	elif palpite < número_aleatório:
-		print(Fore.RED + Style.BRIGHT + 'Chute baixo! Tente denovo: ')
+	elif palpite > numero_aleatorio:
+		print(Fore.RED + Style.BRIGHT + 'Hmmm... Chutou alto demais!!! Tente novamente:\n')
+	elif palpite < numero_aleatorio:
+		print(Fore.RED + Style.BRIGHT + 'Chute baixo! Tente denovo:\n')
 		
 # Finalização
 		
 while True:
-						if palpite == número_aleatório or tentativas >=3:
-							inicializacao = input(Fore.WHITE + Style.NORMAL + 'Deseja reinicar o jogo ? ')
-							espaco()
-							
-						if inicializacao == 'n' or inicializacao == 'não':
-							print(Fore.CYAN + 'Obrigado por jogar! ')
-							break;
+    if palpite == numero_aleatorio or tentativas >=3:
+        inicializacao = input(Fore.WHITE + Style.NORMAL + '\nDeseja reinicar o jogo? [S ou N]: ')
+        
+    if inicializacao == 'N' or inicializacao == 'n' or inicializacao == 'nao':
+        print(Fore.CYAN + '\nObrigado por jogar!\n')
+        break;
 							
 # Reinicialização					
 						
-						elif inicializacao == 's' or inicializacao == 'sim':
-							
-							print(Fore.CYAN + 'REINICIANDO JOGO... ')
-							sleep( 0.7 )
-							limpar_terminal()
-							restart_program()
+    elif inicializacao == 'S' or inicializacao == 's' or inicializacao == 'sim':
+        print(Fore.CYAN + '\nREINICIANDO JOGO...')
+        sleep(0.7)
+        limpar_terminal()
+        restart_program()
